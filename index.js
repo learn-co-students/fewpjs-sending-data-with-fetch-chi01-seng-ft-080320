727146fe126f17fdf22e76767908a584b505a697
Ramazan774 <ramazan.ramazanov@glion.ch>
2020-09-22 19:27:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    const submitData = {
+        name,
+        email
+    };
+    
+    const reqObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(submitData)
+    };
+    
+    return fetch('http://localhost:3000/users', reqObj)
+        .then(resp => resp.json())
+        .then(function (object) {
+            document.body.innerHTML = object['id']
+        })
+        .catch(function(error){
+            document.body.innerHTML = error.message
+        });
+}    
